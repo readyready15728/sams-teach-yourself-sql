29bd2290e5777b18922d36ec45b420cb9089775a
Lynn Bradshaw <readyready15728@gmail.com>
2021-01-15 05:42:40 -0500

    Adding chapter 7 solutions

---
+++ 07/1.sql
@@ -0,0 +1,1 @@
+SELECT vend_id, vend_name AS vname, vend_address AS vaddress, vend_city AS vcity FROM Vendors ORDER BY vname;
---
+++ 07/2.sql
@@ -0,0 +1,1 @@
+SELECT prod_id, prod_price, 0.9 * prod_price AS sale_price FROM Products;
